07982600adf42ac585ec4cbbe9f8d0b4a70202b5
Frederik Gahr <10232152+fgahr@users.noreply.github.com>
2020-07-27 21:35:16 +0200

    Draft shortest_path routine.

--- include/graphd/graph.hpp
+++ include/graphd/graph.hpp
@@ -7,29 +7,33 @@
 
 namespace graphd {
 using NodeName = std::string;
+using DistMap = std::unordered_map<NodeName, double>;
 
 struct Node {
   NodeName name;
-  std::unordered_map<NodeName, double> neighbors;
+  DistMap neighbors;
   Node(NodeName name);
   void add_neighbor(NodeName n, double edge_weight);
 };
 
 struct Path {
-  double total_weight;
+  double total_distance;
   std::vector<NodeName> nodes;
 };
 
 class Graph {
 public:
-  Path shortest_path_between(NodeName a, NodeName b);
+  Path shortest_path(NodeName from, NodeName to);
   void set_name(std::string name);
-  void add_edge(NodeName n1, NodeName n2, double weight = 1.0);
+  void add_edge(NodeName n1, NodeName n2, double distance = 1.0);
 
 private:
+  void dijkstra(NodeName from, NodeName to);
+  Path follow_shortest_path(NodeName from, NodeName to);
   // NOTE: Might well be useless for now.
   std::string name;
-  std::unordered_map<const NodeName, Node> nodes;
+  std::unordered_map<NodeName, Node> nodes;
+  DistMap distances;
 };
 }; // namespace graphd
 
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,15 +1,114 @@
 #include <graphd/graph.hpp>
 
+#include <algorithm>
+#include <limits>
 #include <stdexcept>
 
 namespace graphd {
 
 Node::Node(NodeName n) : name{n} {}
 
-void Node::add_neighbor(NodeName n, double edge_weight) {
-  // FIXME: If the same neighbor is added several times, the connection with
-  // lowest weight should be kept.
-  neighbors[n] = edge_weight;
+// NOTE: map.contains() not available in C++17
+template <typename K, typename V>
+static bool contains(std::unordered_map<K, V> &map, K key) {
+  return map.find(key) != map.end();
+}
+
+static double keep_shortest(DistMap &map, NodeName n, double dist) {
+  if (contains(map, n)) {
+    double current_dist = map[n];
+    if (dist < current_dist) {
+      map[n] = dist;
+      return dist;
+    } else {
+      return current_dist;
+    }
+  } else {
+    map[n] = dist;
+    return dist;
+  }
+}
+
+void Node::add_neighbor(NodeName n, double distance) {
+  keep_shortest(neighbors, n, distance);
+}
+
+void Graph::dijkstra(NodeName from, NodeName to) {
+  DistMap next_step;
+
+  while (!contains(distances, to)) {
+    double shortest_next = std::numeric_limits<double>::max();
+    NodeName next = "";
+    next_step.clear();
+    for (auto [n, dist] : distances) {
+      for (auto [neigh, ndist] : nodes[n].neighbors) {
+        if (contains(distances, neigh)) {
+          continue;
+        }
+
+        double d = keep_shortest(next_step, neigh, dist + ndist);
+        if (d < shortest_next) {
+          shortest_next = d;
+          next = neigh;
+        }
+      }
+    }
+
+    if (next == "") {
+      throw std::runtime_error{"nodes not connected: " + a + ", " + b};
+    }
+    distances[next] = shortest_next;
+  }
+}
+
+Path Graph::follow_shortest_path(NodeName from, NodeName to) {
+  // We start from the end of the path and work our way back.
+  // This means  we are gathering hops back to front and need to reverse the
+  // list later.
+  NodeName current_hop = to;
+  std::vector<NodeName> hops;
+
+  while (current_hop != from) {
+    hops.push_back(current_hop);
+
+    NodeName next_hop = "";
+    double dist = std::numeric_limits<double>::max();
+
+    for (auto [neigh, _] : nodes[current_hop].neighbors) {
+      if (contains(distances, neigh) && distances[neigh] < dist) {
+        next_hop = neigh;
+        dist = distances[neigh];
+      }
+    }
+
+    if (next_hop == "") {
+      throw std::logic_error{"unable to reconstruct shortest path"};
+    }
+    current_hop = next_hop;
+  }
+  hops.push_back(from);
+
+  std::reverse(hops.begin(), hops.end());
+
+  return Path{distances[to], hops};
+}
+
+Path Graph::shortest_path(NodeName from, NodeName to) {
+  distances.clear();
+  if (!contains(nodes, from)) {
+    throw std::runtime_error{"no such node: " + from};
+  }
+  if (!contains(nodes, to)) {
+    throw std::runtime_error{"no such node: " + to};
+  }
+
+  if (from == to) {
+    return Path{0.0, {from}};
+  }
+
+  DistMap distances = {{from, 0.0}};
+  dijkstra(from, to);
+  return follow_shortest_path(from, to);
 }
 
 void Graph::set_name(std::string name) { this->name = name; }
